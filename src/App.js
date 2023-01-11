049bd987f6ebbe1d7a8859f5a854797fee2a9a78
djihane Ghilani <djihaneghilani@gmail.com>
2023-01-11 20:11:43 +0100

    added components

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import { Box } from "@mui/material";
 
 import "./App.css";
 
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
-    <Box width="400px">
-      Navbar
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
   );
 };
