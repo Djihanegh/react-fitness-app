4c2144876ecb0041e4e1fe15bf9985baee1afaba
djihane Ghilani <djihaneghilani@gmail.com>
2023-01-11 19:39:42 +0100

    added App.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { ReactDOM } from "react-dom";
+import { ReactDOM } from "react";
+
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
